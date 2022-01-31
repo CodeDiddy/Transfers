a13ec000b103f0411b8af4e1b52e0500742163c2
CodeDiddy <nielsbakker1978@gmail.com>
2022-01-31 16:05:33 +0100

    omzetten transfersommen naar float

--- app.py
+++ app.py
@@ -14,6 +14,20 @@ import os
 os.environ['WDM_LOG_LEVEL'] = '0'
 # driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
 
+def transfersom(bedrag):
+    if (bedrag == None) | (bedrag == '?') | (type(bedrag) == float):
+        return None
+    elif (bedrag == 'transfervrij') | (bedrag == '-'):
+            return 0
+    else:
+        gesplitst = bedrag.split()
+        if gesplitst[1] == 'dzd.':
+            return float(gesplitst[0]) * 1000
+        elif gesplitst[1] ==  'mln.':
+            return float(gesplitst[0].replace(',','.')) * 1000000
+        else:
+            return None
+
 def haal_transfers(jaar):
    try:
       driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
@@ -49,7 +63,8 @@ def haal_transfers(jaar):
                   
                   temp_list.append(rows['Leeftijd'])
                   temp_list.append(rows['Pos'])
-                  temp_list.append(rows['Afkoopsom'])
+                  # temp_list.append(rows['Afkoopsom'])
+                  temp_list.append(transfersom(rows['Afkoopsom']))
 
                   player_list.append(temp_list)
                   temp_list = []           
@@ -57,7 +72,7 @@ def haal_transfers(jaar):
             continue
                       
       driver.close()
-      with open('gegevens.csv', 'a') as f:
+      with open('transfers.csv', 'a') as f:
          writer = csv.writer(f)
          for player in player_list:
             writer.writerow(player)
@@ -65,6 +80,25 @@ def haal_transfers(jaar):
       print("er is iets mis gegaan")
       driver.close()
       haal_transfers(jaar)
+
+# Standen van deze jaren ophalen
+# def haal_standen(jaar):
+#    try:
+#       driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+#       # driver = webdriver.Firefox()
+
+#       # Ga naar webpagina
+#       driver.get(f"https://www.transfermarkt.nl/eredivisie/tabelle/wettbewerb/NL1/saison_id/{jaar}")
+
+#       # Switch naar Iframe met privacy-voorwaarden, accept en terug naar main content
+#       driver.implicitly_wait(20)
+#       driver.switch_to.frame(driver.find_element(By.ID, 'sp_message_iframe_575848'))
+#       driver.implicitly_wait(20)
+#       driver.find_element(By.CSS_SELECTOR, '.sp_choice_type_11').click()
+#       time.sleep(3)
+#       driver.switch_to.default_content()
+#       time.sleep(3)
+
 
 
 for jaar in range(2000, 2021):
